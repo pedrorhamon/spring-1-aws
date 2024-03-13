b8fb9f3c62d26ea27ce9132609e9565713eb1397
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-03-13 19:23:18 -0300

    adicionando anotações

--- src/main/java/com/starking/realmeet/model/Atendimento.java
+++ src/main/java/com/starking/realmeet/model/Atendimento.java
@@ -1,8 +1,27 @@
 package com.starking.realmeet.model;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
 /**
  * @author pedroRhamon
  */
-public class Atendimento {
+
+@Data
+@Entity
+@Table(name = "atendimento")
+public class Atendimento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 }
