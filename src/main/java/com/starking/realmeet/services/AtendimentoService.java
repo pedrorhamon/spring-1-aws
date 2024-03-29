817fed68c318c02e6f833cae3221bf544fa627bc
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-03-29 14:18:34 -0300

    instanciando o repository

--- src/main/java/com/starking/realmeet/services/AtendimentoService.java
+++ src/main/java/com/starking/realmeet/services/AtendimentoService.java
@@ -2,11 +2,18 @@ package com.starking.realmeet.services;
 
 import org.springframework.stereotype.Service;
 
+import com.starking.realmeet.repositories.AtendimentoRepository;
+
+import lombok.AllArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
 
 @Service
+@AllArgsConstructor
 public class AtendimentoService {
+	
+	private final AtendimentoRepository repository;
 
 }
