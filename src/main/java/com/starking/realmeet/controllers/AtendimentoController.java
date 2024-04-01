b9bb75d8ac4f2cf6fbe097a21f6e693942819407
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-03-31 22:24:26 -0300

    adicionando o metodo no controller buscando todos

--- src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
+++ src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
@@ -1,8 +1,11 @@
 package com.starking.realmeet.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.starking.realmeet.model.Atendimento;
 import com.starking.realmeet.services.AtendimentoService;
 
 import lombok.AllArgsConstructor;
@@ -16,4 +19,8 @@ import lombok.AllArgsConstructor;
 public class AtendimentoController {
 
 	private final AtendimentoService atendimentoService;
+	
+	public Page<Atendimento> findAll(Pageable pageable) {
+		return this.atendimentoService.findAll(pageable);
+	}
 }
