ed1c68f62dc465f537a7fdedc7bdcc19566c44a6
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-04-02 18:07:49 -0300

    criando metodo de atualizar

--- src/main/java/com/starking/realmeet/services/AtendimentoService.java
+++ src/main/java/com/starking/realmeet/services/AtendimentoService.java
@@ -1,5 +1,7 @@
 package com.starking.realmeet.services;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,17 +19,34 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class AtendimentoService {
-	
+
 	private final AtendimentoRepository repository;
-	
+
 	public Page<Atendimento> findAll(Pageable pageable) {
 		return this.repository.findAll(pageable);
 	}
-	
+
 	public void findById(final Long id) {
 		this.repository.findById(id);
 	}
-	
+
+	public Atendimento update(Atendimento atendimentoAtualizado, Long id) {
+		Optional<Atendimento> atendimentoExistente = this.repository.findById(id);
+
+		if (atendimentoExistente.isPresent()) {
+			Atendimento atendimento = atendimentoExistente.get();
+
+			atendimento.setNome(atendimentoAtualizado.getNome());
+			atendimento.setEmail(atendimentoAtualizado.getEmail());
+			atendimento.setCpf(atendimentoAtualizado.getCpf());
+			atendimento.setTelefone(atendimentoAtualizado.getTelefone());
+			
+			return this.repository.save(atendimento);
+		} else {
+			throw new AtendimentoException("Atendimento com o ID " + id + " não encontrado.");
+		}
+	}
+
 	@Transactional
 	public Atendimento save(Atendimento atendimento) {
 		return this.repository.save(atendimento);
