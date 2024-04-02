b41f548e602219562fe01c118307adcd43e37a93
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-04-02 18:12:37 -0300

    criando controlador de update

--- src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
+++ src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
@@ -2,11 +2,13 @@ package com.starking.realmeet.controllers;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,18 @@ public class AtendimentoController {
 	@PostMapping
 	public ResponseEntity<Atendimento> save(@RequestBody @Validated Atendimento atendimento) {
 		return ResponseEntity.created(null).body(this.atendimentoService.save(atendimento));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Atendimento> update(@PathVariable("id") Long id, @RequestBody Atendimento atendimentoAtualizado) {
+	    try {
+	        Atendimento atendimento = atendimentoService.update(atendimentoAtualizado, id);
+	        return ResponseEntity.ok(atendimento);
+	    } catch (RuntimeException e) {
+	        return ResponseEntity.notFound().build();
+	    } catch (Exception e) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	    }
 	}
 
 }
