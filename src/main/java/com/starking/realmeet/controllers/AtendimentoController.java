376d0f1af532264b2b0b4e5d98fffd34e48f6d0c
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-04-01 22:13:29 -0300

    criando endpoint de salvar

--- src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
+++ src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
@@ -2,7 +2,10 @@ package com.starking.realmeet.controllers;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.realmeet.model.Atendimento;
@@ -19,8 +22,13 @@ import lombok.AllArgsConstructor;
 public class AtendimentoController {
 
 	private final AtendimentoService atendimentoService;
-	
+
 	public Page<Atendimento> findAll(Pageable pageable) {
 		return this.atendimentoService.findAll(pageable);
 	}
+
+	public ResponseEntity<Atendimento> save(@RequestBody Atendimento atendimento) {
+		return ResponseEntity.created(null).body(this.atendimentoService.save(atendimento));
+	}
+
 }
