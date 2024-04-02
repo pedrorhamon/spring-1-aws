a09601bf07c7201d2248c204745afba90a0903c9
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-04-01 22:17:57 -0300

    criando o buscador por id

--- src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
+++ src/main/java/com/starking/realmeet/controllers/AtendimentoController.java
@@ -3,9 +3,11 @@ package com.starking.realmeet.controllers;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.realmeet.model.Atendimento;
@@ -27,7 +29,12 @@ public class AtendimentoController {
 		return this.atendimentoService.findAll(pageable);
 	}
 
-	public ResponseEntity<Atendimento> save(@RequestBody Atendimento atendimento) {
+	@GetMapping("{id}")
+	public void findById(@PathVariable("id") Long id) {
+		this.atendimentoService.findById(id);
+	}
+	
+	public ResponseEntity<Atendimento> save(@RequestBody @Validated Atendimento atendimento) {
 		return ResponseEntity.created(null).body(this.atendimentoService.save(atendimento));
 	}
 
